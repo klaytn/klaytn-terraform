94e56a145e321f7d02ebe77bf916cb72acd1ffe1
Tony Lee <52311277+iv0rish@users.noreply.github.com>
2022-03-18 12:04:48 +0900

    Add node's public ip to terraform output

--- service-chain-aws/deploy-4scn/output.tf
+++ service-chain-aws/deploy-4scn/output.tf
@@ -1,8 +1,16 @@
-output "scn_ip_addr" {
+output "scn_private_ip_addr" {
   description = "SCN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
   value       = ["${aws_instance.scn.*.private_ip}"]
 }
-output "en_ip_addr" {
+output "scn_public_ip_addr" {
+  description = "SCN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_eip.scn.*.public_ip}"]
+}
+output "en_private_ip_addr" {
   description = "EN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
   value       = ["${aws_instance.en.*.private_ip}"]
+}
+output "en_public_ip_addr" {
+  description = "EN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_eip.en.*.public_ip}"]
 }
