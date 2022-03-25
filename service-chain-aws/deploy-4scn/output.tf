47e877dc991e8e1dfaf4b063fb6fa48c48632217
Sawyer <1993lsy@hanmail.net>
2022-03-25 16:42:25 +0900

    Fix typo

--- service-chain-aws/deploy-4scn/output.tf
+++ service-chain-aws/deploy-4scn/output.tf
@@ -3,7 +3,7 @@ output "scn_private_ip_addr" {
   value       = ["${aws_instance.scn.*.private_ip}"]
 }
 output "scn_public_ip_addr" {
-  description = "SCN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  description = "SCN instance nodes' public ip. It might help create inventory file when it comes to use ansible"
   value       = ["${aws_eip.scn.*.public_ip}"]
 }
 output "en_private_ip_addr" {
@@ -11,6 +11,6 @@ output "en_private_ip_addr" {
   value       = ["${aws_instance.en.*.private_ip}"]
 }
 output "en_public_ip_addr" {
-  description = "EN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  description = "EN instance nodes' public ip. It might help create inventory file when it comes to use ansible"
   value       = ["${aws_eip.en.*.public_ip}"]
 }
