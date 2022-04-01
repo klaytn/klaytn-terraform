1fb8b6d75e49d7e93bfc0eef40b29b2bbb751b64
Tony Lee <52311277+iv0rish@users.noreply.github.com>
2022-04-01 16:24:13 +0900

    Add Mainnet IP as output

--- service-chain-aws/deploy-L1-L2/output.tf
+++ service-chain-aws/deploy-L1-L2/output.tf
@@ -17,6 +17,24 @@ output "sen_public_ip_addr" {
   value       = ["${aws_eip.sen.*.public_ip}"]
 }
 
+output "cn_private_ip_addr" {
+  description = "CN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_instance.cn.*.private_ip}"]
+}
+output "cn_public_ip_addr" {
+  description = "CN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_eip.cn.*.public_ip}"]
+}
+
+output "pn_private_ip_addr" {
+  description = "PN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_instance.pn.*.private_ip}"]
+}
+output "pn_public_ip_addr" {
+  description = "PN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_eip.pn.*.public_ip}"]
+}
+
 output "en_private_ip_addr" {
   description = "EN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
   value       = ["${aws_instance.en.*.private_ip}"]
