e18816aba5697f00aa5e1339ed3056e233a42d10
Tony Lee <52311277+iv0rish@users.noreply.github.com>
2022-04-04 15:18:26 +0800

    Initial commit

---
+++ service-chain-aws/deploy-multi_layer-servicechain/output.tf
@@ -0,0 +1,18 @@
+output "scn_private_ip_addr" {
+  description = "SCN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_instance.scn.*.private_ip}"]
+}
+output "scn_public_ip_addr" {
+  description = "SCN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_eip.scn.*.public_ip}"]
+}
+
+output "spn_public_ip_addr" {
+  description = "SCN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_eip.spn.*.public_ip}"]
+}
+
+output "sen_public_ip_addr" {
+  description = "SCN instance nodes' private ip. It might help create inventory file when it comes to use ansible"
+  value       = ["${aws_eip.sen.*.public_ip}"]
+}
